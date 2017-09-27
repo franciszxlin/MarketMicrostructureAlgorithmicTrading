89b7268a52f80b7a5d4369121d78574e179c4afc
moonRockTrader <32208323+moonRockTrader@users.noreply.github.com>
2017-09-27 01:15:19 -0400

    commit1
    catching up homework 1

---
+++ hw1.R
@@ -0,0 +1,26 @@
+library(rkdb)
+h1<-open_connection('172.19.10.167',6005)
+h2<-open_connection('172.19.10.167',7005)
+# Problem 1 
+
+# Part (a)
+execute(h1,'select name from instinfo where inst like "PA"')
+# The product whose instrument code is PA is Palladium contract
+execute(h1,'select name from instinfo where inst like "ZC"')
+# The product whose instrument code is ZC is Corn contract
+
+# Part (b)
+execute(h1,'select from instinfo where inst like "RB"')
+execute(h1,'select contractPrc:15875*notional from instinfo where inst like "RB"')
+# The actual contract price is 66675$
+execute(h1,'select realPrc:15875*dispfactor from instinfo where inst like "RB"')
+# The price for gasoline in dollars per gallon is 1.5875$/gallon
+
+# Part (c)
+execute(h1, 'select dailyMargin:(15876-15875)*notional from instinfo where inst like "RB"')
+# The clearing house will deposit $4.2 in your account
+
+# Problem 2
+execute(h1, '(update inst:sym2inst[sym] from 5 # `volume xdesc select volume:sum siz by sym from trade where date=2017.08.18) lj select name by inst from instinfo')
+# The 5 most actively traded interest rates products on August 18, 2017 are 10 Yr Note, 5 Yr Note, U.S. Treasury Bond, 2 Yr Note, Eurodollar contracts
+execute()
